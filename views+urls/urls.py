dec69c5f26579c4e8e8c3ffe91255b68ec7ce0a3
larkingz01 <73679235+larkingz01@users.noreply.github.com>
2020-12-15 17:41:31 +0700

    Delete urls.py

--- views+urls/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import * #ดึงฟังก์ชัน HomePage มาทำงาน
-
-urlpatterns = [
-	# localhost:8000/
-	path('',HomePage, name='home-page'),
-	path('register/', Register, name='register-page')
-]
